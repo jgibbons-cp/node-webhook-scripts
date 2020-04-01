b806bedf6141be8132c369285919cb1806073150
jenks.gibbons <jenks.gibbons@COMP10581.local>
2020-03-31 19:44:31 -0700

    cleanup

--- configure_host_for_test.sh
+++ configure_host_for_test.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 #install stress
-#ssh $USER@$HOST "sudo amazon-linux-extras install epel -y ; sudo yum install stress -y"
+ssh $USER@$HOST "sudo amazon-linux-extras install epel -y ; sudo yum install stress -y"
 
 #install Datadog agent
-#scp $ENV_VARS $USER@$HOST:/tmp
-#ssh $USER@$HOST "source /tmp/$ENV_VARS ; curl -O https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh ; sh install_script.sh ; rm /tmp/$ENV_VARS"
+scp $ENV_VARS $USER@$HOST:/tmp
+ssh $USER@$HOST "source /tmp/$ENV_VARS ; curl -O https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh ; sh install_script.sh ; rm /tmp/$ENV_VARS"
 
 #configure process collection and restart the agent
-#scp datadog.yaml $USER@$HOST:/tmp
-#ssh $USER@$HOST "sed -i 's/datadog_api_key/$DD_API_KEY/g' /tmp/datadog.yaml"
-#ssh $USER@$HOST "sudo mv /tmp/datadog.yaml /etc/datadog-agent/datadog.yaml ; sudo service datadog-agent restart"
+scp datadog.yaml $USER@$HOST:/tmp
+ssh $USER@$HOST "sed -i 's/datadog_api_key/$DD_API_KEY/g' /tmp/datadog.yaml"
+ssh $USER@$HOST "sudo mv /tmp/datadog.yaml /etc/datadog-agent/datadog.yaml ; sudo service datadog-agent restart"
 
 #run stress
-#ssh -f $USER@$HOST "nohup stress --cpu 3 &>/dev/null"
+ssh -f $USER@$HOST "nohup stress --cpu 3 &>/dev/null"
 
 #get instance ID
-#ssh $USER@$HOST curl -O http://169.254.169.254/latest/meta-data/$INSTANCE_ID_STORE
-#scp $USER@$HOST:/home/$USER/$INSTANCE_ID_STORE .
+ssh $USER@$HOST curl -O http://169.254.169.254/latest/meta-data/$INSTANCE_ID_STORE
+scp $USER@$HOST:/home/$USER/$INSTANCE_ID_STORE .
 
 #create webhook
 curl -v -X POST \
